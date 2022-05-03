3289cef2efb23be713d1711714ffb54477cd20cf
luc <luc.bourrat.42@gmail.com>
2022-05-03 15:29:27 +0200

    Bug fixed (retrieval of the training date) after OC FE Update

--- students---studentID---dashboard/style.js
+++ students---studentID---dashboard/style.js
@@ -4,7 +4,8 @@ function observerStudentsElementLoading() {
 	let observer = new MutationObserver(mCallback);
 	
 	function mCallback(mutations) {
-		let currentStudentName = document.getElementById("STUDENT-DASHBOARD-REACT").getElementsByTagName("h1")[0].textContent;
+		let currentStudentName = document.getElementById("student_details_screen").getElementsByTagName("h1")[0].textContent;
+		console.log("Student name =>" + currentStudentName);
 		if (currentStudentName) {
 		    observer.disconnect();
 		    setStudentFormationDate(currentStudentName);
@@ -21,13 +22,21 @@ function setStudentFormationDate(currentStudentName) {
 	
 	for (student of studentsListTab) {
 		if (student.name == currentStudentName) {
-			
-			if (document.getElementsByClassName("mentorshipStudent")[0].getElementsByTagName("h3")[0]) {
-				let studentFormationDateSentence = document.getElementsByClassName("mentorshipStudent")[0].getElementsByTagName("h3")[0].textContent;
-				
+			let financementType = document.getElementById("details").childNodes[0].childNodes[2].childNodes[0].childNodes[0].childNodes[2].textContent.substr(11);
+			console.log(financementType);
+			// Financé par un tiers
+			// Auto-financé
+			if (financementType == "Financé par un tiers") {
+			// if (document.getElementsByClassName("mentorshipStudent")[0].getElementsByTagName("h3")[0]) {
+				// let studentFormationDateSentence = document.getElementsByClassName("mentorshipStudent")[0].getElementsByTagName("h3")[0].textContent;
+				let studentFormationDateSentence = document.getElementById("details").childNodes[0].childNodes[2].childNodes[0].childNodes[0].childNodes[3].textContent.substr(26);
 				let studentFormationDateDay = leftFillNum(parseInt(studentFormationDateSentence.split(/(\d+)/)[1], 10),2);
 				let studentFormationDateMonth = studentFormationDateSentence.split(/(\d+)/)[2].replace(/ /g, '').toLowerCase();
 				let studentFormationDateYear = parseInt(studentFormationDateSentence.split(/(\d+)/)[3], 10);
+				console.log("Date =>" + studentFormationDateSentence);
+				console.log("Day =>" + studentFormationDateDay);
+				console.log("Month =>" + studentFormationDateMonth);
+				console.log("Year =>" + studentFormationDateYear);
 				
 				let months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
 				
@@ -35,7 +44,7 @@ function setStudentFormationDate(currentStudentName) {
 				while (i < months.length) {
 					if (studentFormationDateMonth == months[i]) {
 						studentFormationDateMonth = leftFillNum(i+1,2);
-						console.log("WWWWWWWWWWW =>" + studentFormationDateMonth);
+						console.log("Fin de formation au mois numéro =>" + studentFormationDateMonth);
 						break
 					}
 					i++;
@@ -44,9 +53,12 @@ function setStudentFormationDate(currentStudentName) {
 				student.formationDate = studentFormationDateYear + "-" + studentFormationDateMonth + "-" + studentFormationDateDay;
 				localStorage.setItem('studentsListTab', JSON.stringify(studentsListTab));
 			}
-			else {
+			else if (financementType == "Auto-financé") {
 				student.formationDate = "AF";
 				localStorage.setItem('studentsListTab', JSON.stringify(studentsListTab));
+			}
+			else {
+				console.log("!!! Problème avec la récupération du type de financement (financementType) !!!");
 			}
 			
 			break;
